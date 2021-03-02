c90985370c76118d4f98632eeb84c82157b634b9
Logan Markle <loganmarkle@gmail.com>
2021-03-02 21:24:25 +0000

    added ability to remove playlists after they have been added, shows if a playlist is selected or not

--- code/src/components/spotify-components.jsx
+++ code/src/components/spotify-components.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 // import { get, post, put } from '../js/httpRequests';
-import { get,post } from '../js/httpRequests';
+import { get, post } from '../js/httpRequests'
 
 
 export class Main extends React.Component
 {
 
-	constructor(props)
+	constructor (props)
 	{
 		super(props)
 		this.getPlaylists = this.getPlaylists.bind(this)
@@ -14,9 +14,8 @@ export class Main extends React.Component
 		this.state = {
 			playlists: [],
 			username: "loading",
-			selectedPlaylists: [],
-			songs: [],
-			selectedSongs: []
+			selectedPlaylists: new Set(),
+			songs: new Set(),
 		}
 	}
 
@@ -36,7 +35,6 @@ export class Main extends React.Component
 		let playlist = await get(url, null, null)
 		this.setState({
 			playlists: playlist.items,
-			// playlists: [...this.state.playlists, ...tmp],
 			username: user.display_name,
 		})
 	}
@@ -44,28 +42,43 @@ export class Main extends React.Component
 
 	async selectedPlaylist(plid)
 	{
-		// TODO:
-		//see if playlist is already selectedPlaylists
-		//if it is then uncheck it
-		let url = "https://api.spotify.com/v1/playlists/" + plid
 
+		let url = "https://api.spotify.com/v1/playlists/" + plid
 		let pl = await get(url, null, null)
-		var tmp = this.state.selectedPlaylists
-		tmp.push(pl)
-		this.setState({
-			selectedPlaylists: tmp
-		})
-		tmp = this.state.songs
-		for (var song of pl.tracks.items)
+		var tmp = []
+		for (let song of pl.tracks.items)
 		{
 			tmp.push(song)
 		}
+
+		var tmpPlaylist = this.state.selectedPlaylists
+		var tmpSongList = this.state.songs
+		if (tmpPlaylist.has(plid))
+		{
+			tmpPlaylist.delete(plid)
+			for (let s of tmpSongList)
+			{
+				for (let song of tmp)
+				{
+					if (s.id === song.id)
+					{
+						tmpSongList.delete(s)
+					}
+				}
+			}
+		} else
+		{
+			tmpPlaylist.add(plid)
+			for (let song of tmp)
+			{
+				tmpSongList.add(song)
+			}
+		}
 		this.setState({
-			songs: tmp
+			selectedPlaylists: tmpPlaylist,
+			songs: tmpSongList
 		})
 	}
-
-
 
 
 	render()
@@ -75,7 +88,7 @@ export class Main extends React.Component
 				<div className="container-fluid sticky-top mx-2 py-5 top-playlists">
 					<h1>Hello { this.state.username }</h1>
 					<div className="row playlists">
-						{ this.state.playlists.length > 0 && <Playlists playlists={ this.state.playlists } selectedPlaylist={ this.selectedPlaylist } /> }
+						{ this.state.playlists.length > 0 && <Playlists playlists={ this.state.playlists } selectedPlaylist={ this.selectedPlaylist } selectedPlaylistList={ this.state.selectedPlaylists } /> }
 					</div>
 				</div>
 				<div className="container overflow-auto">
@@ -85,29 +98,22 @@ export class Main extends React.Component
 				</div>
 			</div>
 
-		);
+		)
 	}
 }
 
 export class Playlists extends React.Component
 {
-	constructor(props)
+	constructor (props)
 	{
 		super(props)
-
 	}
+
 
 	render()
 	{
-		if (this.state === undefined)
-		{
-			return (
-				<h1>uiop</h1>
-			)
-		}
 		return (
 			<div className="card-deck" id="playlist_list">
-				<h4>my playlists</h4>
 				{this.props.playlists.map((p) =>
 				{
 					return (
@@ -122,28 +128,33 @@ export class Playlists extends React.Component
 
 export class Playlist extends React.Component
 {
-	constructor(props)
+	constructor (props)
 	{
 		super(props)
-		this.handleClick = this.handleClick.bind(this);
-
+		this.handleClick = this.handleClick.bind(this)
+		this.state = {
+			active: false
+		}
 	}
 
 
 	handleClick()
 	{
 		this.props.selectedPlaylist(this.props.id)
+		this.setState({
+			active: !this.state.active
+		})
 	}
 	render()
 	{
 
 		return (
-			<a className="card card-block playlist_card playlist_card_inactive col-4 col-sm-4 col-md-3 col-lg-2 col-xxl-1 text-truncate" id={ this.props.id } href="#" onClick={ this.handleClick }>
+			<a className={ `card card-block playlist_card  col-4 col-sm-4 col-md-3 col-lg-2 col-xxl-1 text-truncate ${this.state.active ? "playlist_card_active" : "playlist_card_inactive"}` } id={ this.props.id } href="#" onClick={ this.handleClick }>
 				<img src={ this.props.artwork } className="card-img-top border-0" alt="..." />
 				<div className="card-body">
 					<h5 className="card-title">{ this.props.name }</h5>
 					<p className="card-text">
-						<span className="badge badge-pill badge-secondary"> { this.props.total }</span>
+						<span className="badge badge-pill badge-secondary">{ this.props.total }</span>
 					</p>
 				</div>
 			</a>
@@ -154,45 +165,41 @@ export class Playlist extends React.Component
 
 export class SongList extends React.Component
 {
-	constructor(props)
+	constructor (props)
 	{
 		super(props)
-
 	}
 	render()
 	{
-		if (this.state === undefined)
-		{
-			return (
-				<h1>uiop</h1>
-			)
-		}
 		return (
-				<div className="col-sm overflow-auto" id="selected_song_list">
-				{this.props.songs.map((s) =>
+			<div className="col-sm overflow-auto" id="selected_song_list">
+				{Array.from(this.props.songs).map((s) =>
 				{
 					return (
 						<Song name={ s.track.name } id={ s.track.id } is_local={ s.track.is_local } />
 					)
 				}) }
-				</div>
+			</div>
 		)
 	}
 }
 
 export class Song extends React.Component
 {
-	constructor(props)
+	constructor (props)
 	{
 		super(props)
-		this.handleClick = this.handleClick.bind(this);
+		this.handleClick = this.handleClick.bind(this)
 	}
 
 	handleClick()
 	{
-		if (this.props.is_local) {
+		if (this.props.is_local)
+		{
+			console.log("trying to play local song, spotify does not support that yet :(")
 			return
-		} else {
+		} else
+		{
 			let url = "https://api.spotify.com/v1/me/player/queue"
 			url += '?uri=spotify%3Atrack%3A' + this.props.id
 			post(url, null, null)
@@ -200,10 +207,8 @@ export class Song extends React.Component
 	}
 	render()
 	{
-
 		return (
-			<a className="list-group-item list-group-item-action" id={this.props.is_local ? "sn-null" : ""} href="#" onClick={ this.handleClick }>{ this.props.name }</a>
-			// {!this.props.local && <a className="list-group-item list-group-item-action"  href="#" onClick={ this.handleClick }>{ this.props.name }</a>}
+			<a className="list-group-item list-group-item-action" id={ this.props.is_local ? "sn-null" : "" } href="#" onClick={ this.handleClick }>{ this.props.name }</a>
 
 		)
 
